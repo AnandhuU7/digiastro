9bb73a84f0a77ce7990efa568c6875e1017e9212
AnandhuU7 <anandhuachu777@gmail.com>
2025-03-28 14:35:57 +0530

    Production build

--- src/components/Whatsapp.tsx
+++ src/components/Whatsapp.tsx
@@ -1,12 +1,12 @@
 import { FloatingWhatsApp } from 'react-floating-whatsapp'
-import logo from '../assets/images/logo.svg';   
+import logo from '../assets/images/logo.png';   
 
 const Whatsapp = () => {
     const logoUrl = logo.src;
     return <FloatingWhatsApp  
     accountName='Dubai Hikvision' 
     phoneNumber='+971552929644' 
-    avatar={logoUrl}
+    avatar={logoUrl} 
     statusMessage="Live chat now"
     chatMessage="Welcome to Dubai Hikvision 🤝. How can we help?"
     />
