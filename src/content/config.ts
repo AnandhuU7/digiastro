72d4beb28339078cf1463bef5663234c54a20593
AnandhuU7 <anandhuachu777@gmail.com>
2025-04-10 17:29:36 +0530

    Products Updating

--- src/content/config.ts
+++ src/content/config.ts
@@ -161,6 +161,90 @@ const turbohdCollection = defineCollection({
   }),
 });
 
+const dvrCollection = defineCollection({
+  type: 'content',
+  schema: ({ image }) => z.object({
+    id: z.number(),
+    title: z.string(),
+    slug: z.string().optional(),
+    subTitle: z.string(),
+    category: z.string(),
+    imgCard: image(),
+    imgAlt: z.string(),
+    thumbnails: z.array(image()).optional(),
+    features: z.array(z.string()),
+    rating: z.number().min(0).max(5).optional(),
+    reviewCount: z.number().min(0).optional(),
+    specifications: z.record(
+      z.string(),
+      z.record(
+        z.string(),
+        z.union([
+          z.string(),
+          z.null(),
+          z.record(z.string(), z.string()),
+          z.record(z.string(), z.union([z.string(), z.null(), z.record(z.string(), z.string())]))
+        ])
+      )
+    ).optional(),
+  }),
+});
+
+const accessControlCollection = defineCollection({
+  type: 'content',
+  schema: ({ image }) => z.object({
+    id: z.number(),
+    title: z.string(),
+    slug: z.string().optional(),
+    subTitle: z.string(),
+    category: z.string(),
+    imgCard: image(),
+    imgAlt: z.string(),
+    thumbnails: z.array(image()).optional(),
+    features: z.array(z.string()),
+    rating: z.number().min(0).max(5).optional(),
+    reviewCount: z.number().min(0).optional(),
+    specifications: z.record(
+      z.string(),
+      z.record(
+        z.string(),
+        z.union([
+          z.string(),
+          z.null()
+        ])
+      )
+    ).optional(),
+  }),
+});
+
+
+const videoIntercomCollection = defineCollection({
+  type: 'content',
+  schema: ({ image }) => z.object({
+    id: z.number(),
+    title: z.string(),
+    slug: z.string().optional(),
+    subTitle: z.string(),
+    category: z.string(),
+    imgCard: image(),
+    imgAlt: z.string(),
+    thumbnails: z.array(image()).optional(),
+    features: z.array(z.string()),
+    rating: z.number().min(0).max(5).optional(),
+    reviewCount: z.number().min(0).optional(),
+    specifications: z.record(
+      z.string(),
+      z.record(
+        z.string(),
+        z.union([
+          z.string(),
+          z.null()
+        ])
+      )
+    ).optional(),
+  }),
+})
+
 const blogCollection = defineCollection({
   type: "content",
   schema: ({ image }) => z.object ({
@@ -183,4 +267,7 @@ export const collections = {
   'nvr': nvrCollection,
   'explosionproof': explosionproofCollection,
   'turbohd': turbohdCollection,
+  'dvr': dvrCollection,
+  'accesscontrol': accessControlCollection,
+  'videointercom': videoIntercomCollection,
 };
